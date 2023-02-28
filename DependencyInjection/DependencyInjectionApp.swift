7e71210918f6997508bc2ce356f89838c128a974
Vinícius <viniciusfloresribeiro@gmail.com>
2023-02-28 10:14:59 -0300

    create project

---
+++ DependencyInjection/DependencyInjectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DependencyInjectionApp.swift
+//  DependencyInjection
+//
+//  Created by Vinícius Flores Ribeiro on 28/02/23.
+//
+
+import SwiftUI
+
+@main
+struct DependencyInjectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
